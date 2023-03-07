85495716b075a37cd5de393d10d7acf0beb9236b
Mahamad Abdulah <126168630+Mahamadabdulah2025@users.noreply.github.com>
2023-03-07 20:03:12 +0530

    Create Breaking the Records.java

---
+++ Breaking the Records.java
@@ -0,0 +1,74 @@
+/* Maria plays college basketball and wants to go pro. Each season she maintains 
+a record of her play. She tabulates the number of times she breaks her season record for most points and 
+least points in a game. Points scored in the first game establish her record for the season, and she begins counting from there.
+
+Example
+Sample Input 
+9
+10 5 20 20 4 5 2 25 1
+Sample Output 
+2 4
+
+Sample Input 1
+
+10
+3 4 21 36 10 28 35 5 24 42
+Sample Output 1
+
+4 0 */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+    // Write your code here
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] scoresTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> scores = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int scoresItem = Integer.parseInt(scoresTemp[i]);
+            scores.add(scoresItem);
+        }
+
+        List<Integer> result = Result.breakingRecords(scores);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
